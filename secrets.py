39cd03d218d3b566b57c43662bc278ec831055e9
Hiram F <hifarrer@gmail.com>
2021-02-01 23:19:07 -0600

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,6 +1,6 @@
 secrets = {
-        'ssid' : 'YOUR WIFI NAME',
-        'password' : 'your wifi password',
+        'ssid' : 'YOUR WIFEs BOYFRIEND WIFI NAME',
+        'password' : 'wifi password',
         'aio_username': 'adafruit username',
         'aio_key': 'YOUR_ADAFRUIT_API_KEY',
         'stonks_key': 'Your finnhub.io API key'
